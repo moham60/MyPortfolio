081578f0b7a8b8237ce62d9703519982e3ad2fd1
moham60 <alkingg289@gmail.com>
2025-10-27 22:02:19 +0300

    lastUpdate

--- src/app/_components/SectionTiitle/Title.jsx
+++ src/app/_components/SectionTiitle/Title.jsx
@@ -1,11 +1,12 @@
 "use client"
 import { motion } from "framer-motion";
+import { transition } from "../../../../transition";
 export function Title({title}) {
     return (
        <motion.h2
         initial={{ opacity: 0,y:40 }}
-        animate={{ opacity: 1,y:0 }}
-        transition={{ duration: 0.6 }}
+        whileInView={{ opacity: 1,y:0 }}
+        transition={transition}
         className=" text-3xl  sm:text-4xl font-bold text-center mb-10 mt-8
    bg-gradient-to-r from-fuchsia-600 via-purple-600 to-blue-600 dark:from-fuchsia-400 dark:via-purple-400 dark:to-blue-400
     text-transparent bg-clip-text
