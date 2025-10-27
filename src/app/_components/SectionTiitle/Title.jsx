4c2be20f85aaee8bb1e19301a2394b1f3336e3a2
moham60 <alkingg289@gmail.com>
2025-10-27 21:58:49 +0300

    secondUpdate

--- src/app/_components/SectionTiitle/Title.jsx
+++ src/app/_components/SectionTiitle/Title.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 export function Title({title}) {
     return (
        <motion.h2
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0,y:40 }}
+        animate={{ opacity: 1,y:0 }}
         transition={{ duration: 0.6 }}
         className=" text-3xl  sm:text-4xl font-bold text-center mb-10 mt-8
    bg-gradient-to-r from-fuchsia-600 via-purple-600 to-blue-600 dark:from-fuchsia-400 dark:via-purple-400 dark:to-blue-400
